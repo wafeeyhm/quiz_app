38bea69d8d0b44b8070acfcd0201a7062d3cd367
wafeeyhm <wafiy@anak.it>
2021-11-23 21:56:08 +0800

    private properties added to MyAppState

--- lib/main.dart
+++ lib/main.dart
@@ -7,18 +7,18 @@ void main() {
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
-      questionIndex = questionIndex + 1;
+      _questionIndex = _questionIndex + 1;
     });
-    print(questionIndex);
+    print(_questionIndex);
   }
 
   @override
@@ -35,21 +35,19 @@ class MyAppState extends State<MyApp> {
         body: Column(
           children: [
             Text(
-              questionsList[questionIndex],
+              questionsList[_questionIndex],
             ),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: () {
-                print('Answer 3 chosen!');
-              },
+              onPressed: _answerQuestion,
             ),
           ],
         ),
