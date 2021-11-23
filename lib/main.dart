464117f2b9f7e23abc6c831698a065df8e494d68
wafeeyhm <wafiy@anak.it>
2021-11-23 22:16:00 +0800

    added question custom widget and link to main class

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -34,7 +36,7 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(
+            Question(
               questionsList[_questionIndex],
             ),
             RaisedButton(
